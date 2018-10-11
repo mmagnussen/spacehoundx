8791259a751a55879cf17f365b1bb74205660037
Mike <mike.magnussen@lifeway.com>
2018-10-11 14:18:12 -0500

    create express server that opens in 3001

---
+++ buildScripts/server.js
@@ -0,0 +1,24 @@
+
+var express = require('express')
+var path = require('path')
+var open = require('open')
+
+
+//Establish port numner and initialize express
+const port = 3001;
+const app = express();
+
+//Listen on port 3000 and report any error
+app.listen(port, function (error) {
+    if (error) {
+        console.log(error);
+    } else {
+        open(`http://localhost:${port}`)
+    }
+});
+
+//Have express serve a file when you visit localhost:3001
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
